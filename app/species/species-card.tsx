1eaa90dd5d88bba3c5e5244d3cc7eb71eec9c799
itsjackfan <fieryphoenix425@gmail.com>
2024-09-13 12:39:55 -0400

    Update deliverable code and easy stretch goals.

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -10,12 +10,204 @@ on the client-side to correctly match component state and props should the order
 React server components don't track state between rerenders, so leaving the uniquely identified components (e.g. SpeciesCard)
 can cause errors with matching props and state in child components if the list order changes.
 */
+
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/components/ui/use-toast";
+import { createBrowserSupabaseClient } from "@/lib/client-utils";
 import type { Database } from "@/lib/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState, type MouseEvent } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
-export default function SpeciesCard({ species }: { species: Species }) {
+{
+  /* Reused from add-species-dialog.tsx */
+}
+// Define kingdom enum for use in Zod schema and displaying dropdown options in the form
+const kingdoms = z.enum(["Animalia", "Plantae", "Fungi", "Protista", "Archaea", "Bacteria"]);
+
+const speciesSchema = z.object({
+  scientific_name: z
+    .string()
+    .trim()
+    .min(1)
+    .transform((val) => val?.trim()),
+  common_name: z
+    .string()
+    .nullable()
+    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
+    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
+  kingdom: kingdoms,
+  total_population: z.number().int().positive().min(1).nullable(),
+  image: z
+    .string()
+    .url()
+    .nullable()
+    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
+    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
+  description: z
+    .string()
+    .nullable()
+    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
+    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
+});
+
+export default function SpeciesCard({
+  species,
+  sessionId,
+  searchTerm,
+}: {
+  species: Species;
+  sessionId: string;
+  searchTerm?: string;
+}) {
+  // Control whether the dialog is open or closed
+  const [open, setDialogOpen] = useState<boolean>(false);
+  // Control editing mode
+  const [isEditing, setIsEditing] = useState(false);
+  // Control deleted or not
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  // Track image file preview and file data
+  const [selectedImage, setSelectedImage] = useState<string | null>(species.image ?? null);
+
+  // Set default values for the form (on open) to existing data
+  const defaultValues = {
+    scientific_name: species.scientific_name,
+    common_name: species.common_name,
+    kingdom: species.kingdom,
+    total_population: species.total_population,
+    image: species.image,
+    description: species.description,
+  };
+
+  type SpeciesFormValues = z.infer<typeof speciesSchema>;
+
+  const form = useForm<SpeciesFormValues>({
+    resolver: zodResolver(speciesSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const router = useRouter();
+
+  // Utility to find and highlight the search term in the description
+  const highlightSearchTerm = (description: string | null) => {
+    if (!description || !searchTerm) return description;
+
+    const regex = new RegExp(`(.{0,3})(${searchTerm})(.{0,3})`, "gi");
+    return description.replace(regex, (_, before, match, after) => {
+      return `${before}<mark>${match}</mark>${after}`;
+    });
+  };
+
+  const onSubmit = async (data: SpeciesFormValues) => {
+    // Instantiate Supabase client (for client components) and make update based on input data
+    const supabase = createBrowserSupabaseClient();
+    const { error } = await supabase
+      .from("species")
+      .update({
+        scientific_name: data.scientific_name,
+        common_name: data.common_name,
+        kingdom: data.kingdom,
+        total_population: data.total_population,
+        image: data.image,
+        description: data.description,
+      })
+      .eq("id", species.id);
+
+    // Catch and report errors from Supabase and exit the onSubmit function with an early 'return' if an error occurred.
+    if (error) {
+      return toast({
+        title: "Something went wrong.",
+        description: error.message,
+        variant: "destructive",
+      });
+    }
+
+    // Because Supabase errors were caught above, the remainder of the function will only execute upon a successful edit
+    setIsEditing(false);
+
+    // Reset form values to the data values that have been processed by zod.
+    // This is helpful to do after EDITING, so that the user sees any changes that have occurred during transformation
+    form.reset(data);
+
+    // Router.refresh does not affect ProfileForm because it is a client component, but it will refresh the initials in the user-nav in the event of a username change
+    router.refresh();
+
+    return toast({
+      title: "Species updated successfully!",
+    });
+  };
+
+  const handleCancel = (e: MouseEvent) => {
+    e.preventDefault();
+    // If edit canceled, reset the form data to the original values which were set from props
+    form.reset(defaultValues);
+    // Turn off editing mode
+    setIsEditing(false);
+  };
+
+  const startEditing = (e: MouseEvent) => {
+    e.preventDefault();
+    setIsEditing(true);
+  };
+
+  const startDelete = (e: MouseEvent) => {
+    e.preventDefault();
+    setDeleteOpen(true);
+  };
+
+  const confirmDelete = async () => {
+    const supabase = createBrowserSupabaseClient();
+    const { error } = await supabase.from("species").delete().eq("id", species.id);
+
+    // Catch and report errors from Supabase and exit the onSubmit function with an early 'return' if an error occurred.
+    if (error) {
+      return toast({
+        title: "Something went wrong.",
+        description: error.message,
+        variant: "destructive",
+      });
+    }
+
+    // Because Supabase errors were caught above, the remainder of the function will only execute upon a successful edit
+    setDeleteOpen(false);
+
+    // Router.refresh does not affect ProfileForm because it is a client component, but it will refresh the initials in the user-nav in the event of a username change
+    router.refresh();
+
+    return toast({
+      title: "Species deleted successfully!",
+    });
+  };
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files?.[0]) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result as string);
+        form.setValue("image", reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="m-4 w-72 min-w-72 flex-none rounded border-2 p-3 shadow">
       {species.image && (
@@ -23,11 +215,220 @@ export default function SpeciesCard({ species }: { species: Species }) {
           <Image src={species.image} alt={species.scientific_name} fill style={{ objectFit: "cover" }} />
         </div>
       )}
-      <h3 className="mt-3 text-2xl font-semibold">{species.scientific_name}</h3>
-      <h4 className="text-lg font-light italic">{species.common_name}</h4>
-      <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
-      {/* Replace the button with the detailed view dialog. */}
-      <Button className="mt-3 w-full">Learn More</Button>
+      <h3 className="mt-3 text-2xl font-semibold">{species.common_name}</h3>
+      <h4 className="text-lg font-light italic">{species.scientific_name}</h4>
+
+      {/* Highlighted description snippet */}
+      <p dangerouslySetInnerHTML={{ __html: highlightSearchTerm(species.description) }} />
+
+      <Dialog open={open} onOpenChange={setDialogOpen}>
+        <DialogTrigger asChild>
+          <Button className="mt-3 w-full" onClick={() => setDialogOpen(true)}>
+            Learn More
+          </Button>
+        </DialogTrigger>
+
+        <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+          <DialogHeader>
+            <DialogTitle>{species.common_name}</DialogTitle>
+            <DialogDescription>
+              {species.kingdom}, {species.scientific_name}
+            </DialogDescription>
+          </DialogHeader>
+
+          {!isEditing ? (
+            <div>
+              {selectedImage && (
+                <div className="relative mb-4 h-60 w-full">
+                  <Image src={selectedImage} alt={species.scientific_name} fill style={{ objectFit: "cover" }} />
+                </div>
+              )}
+              <p>
+                <strong>Population: {species.total_population}</strong>
+              </p>
+              <br></br>
+              <p>{species.description ?? "No description available."}</p>
+              <br></br>
+              <p>
+                <em>Written by {species?.profiles.display_name ?? "unknown author"}</em>
+              </p>
+            </div>
+          ) : (
+            <Form {...form}>
+              <form onSubmit={(e) => void form.handleSubmit(onSubmit)(e)} className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="scientific_name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Scientific Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Cavia porcellus" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="common_name"
+                  render={({ field }) => {
+                    // We must extract value from field and convert a potential defaultValue of `null` to "" because inputs can't handle null values: https://github.com/orgs/react-hook-form/discussions/4091
+                    const { value, ...rest } = field;
+                    return (
+                      <FormItem>
+                        <FormLabel>Common Name</FormLabel>
+                        <FormControl>
+                          <Input value={value ?? ""} placeholder="Guinea pig" {...rest} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+                <FormField
+                  control={form.control}
+                  name="kingdom"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Kingdom</FormLabel>
+                      <Select onValueChange={(value) => field.onChange(kingdoms.parse(value))} value={field.value}>
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select a kingdom" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectGroup>
+                            {kingdoms.options.map((kingdom, index) => (
+                              <SelectItem key={index} value={kingdom}>
+                                {kingdom}
+                              </SelectItem>
+                            ))}
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="total_population"
+                  render={({ field }) => {
+                    const { value, ...rest } = field;
+                    return (
+                      <FormItem>
+                        <FormLabel>Total population</FormLabel>
+                        <FormControl>
+                          {/* Using shadcn/ui form with number: https://github.com/shadcn-ui/ui/issues/421 */}
+                          <Input
+                            type="number"
+                            value={value ?? ""}
+                            placeholder="300000"
+                            {...rest}
+                            onChange={(event) => field.onChange(+event.target.value)}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => {
+                    // We must extract value from field and convert a potential defaultValue of `null` to "" because textareas can't handle null values: https://github.com/orgs/react-hook-form/discussions/4091
+                    const { value, ...rest } = field;
+                    return (
+                      <FormItem>
+                        <FormLabel>Description</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            value={value ?? ""}
+                            placeholder="The guinea pig or domestic guinea pig, also known as the cavy or domestic cavy, is a species of rodent belonging to the genus Cavia in the family Caviidae."
+                            {...rest}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+                <FormField
+                  control={form.control}
+                  name="image"
+                  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Image</FormLabel>
+                      <FormControl>
+                        <Input type="file" accept="image/*" onChange={handleImageUpload} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <div className="mt-4 flex justify-between">
+                  <Button type="submit">Save</Button>
+                  <Button variant="secondary" onClick={handleCancel}>
+                    Cancel
+                  </Button>
+                </div>
+              </form>
+            </Form>
+          )}
+
+          <div className="mt-4 flex justify-between">
+            {!isEditing ? (
+              <>
+                <Button
+                  onClick={startEditing}
+                  disabled={sessionId !== species.author}
+                  className={`${sessionId !== species.author ? "cursor-not-allowed bg-gray-400" : ""}`}
+                >
+                  Edit
+                </Button>
+
+                <Button
+                  onClick={startDelete}
+                  disabled={sessionId !== species.author}
+                  className={`${sessionId !== species.author ? "cursor-not-allowed bg-gray-400" : ""}`}
+                >
+                  Delete
+                </Button>
+              </>
+            ) : (
+              <></>
+            )}
+            <Button variant="secondary" onClick={() => setDialogOpen(false)}>
+              Close
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+
+      {/* Delete confirmation dialog */}
+      <Dialog open={deleteOpen} onOpenChange={setDeleteOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Confirm Deletion</DialogTitle>
+            <DialogDescription>
+              Are you sure you want to delete this species? This action cannot be undone.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="mt-4 flex justify-between">
+            <Button variant="destructive" onClick={confirmDelete}>
+              Yes, delete.
+            </Button>
+            <Button variant="secondary" onClick={() => setDeleteOpen(false)}>
+              Cancel
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
